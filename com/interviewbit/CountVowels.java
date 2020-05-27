c2570f3be9c7145abcbc1e676d18c8aef432598f
rdaragan80 <46337661+rdaragan80@users.noreply.github.com>
2020-05-27 09:37:49 -0700

    Update CountVowels.java
    large string - 1  3933
    original method time: 4299597644
    large string - 2  3933
    new method time: 8491552

--- com/interviewbit/CountVowels.java
+++ com/interviewbit/CountVowels.java
@@ -41,14 +41,51 @@ public class CountVowels {
 		}
 		return max;
 	}
+	
+	public static int getMaxVowelsInSubstring2(String s, int k) {
+		if (k > s.length()) {
+			return 0;
+		} else if (s.length() == 1) {
+			return vovels.contains(s.charAt(0)) ? 1 : 0;
+		}
+
+		int max = 0;
+		int currentMax = 0;
+
+		// one time only
+		for (int i = 0; i < k; i++) {
+			if (vovels.contains(s.charAt(i))) {
+				currentMax++;
+			}
+		}
+		int ptr1 = 1;
+		int ptr2 = k;
+
+		while (ptr2 < s.length()) {
+			if (vovels.contains(s.charAt(ptr1 - 1))) {
+				currentMax--;
+			}
+			if (vovels.contains(s.charAt(ptr2))) {
+				currentMax++;
+			}
+			max = Math.max(max, currentMax);
+			ptr1++;
+			ptr2++;
+		}
+		return max;
+	}
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("large string - " + getMaxVowelsInSubstring(getStringFromFile(), 20_000));
-		System.out.println("abciiidef - " + getMaxVowelsInSubstring("abciiidef", 3));
-		System.out.println("aeiou - " + getMaxVowelsInSubstring("aeiou", 2));
-		System.out.println("leetcode - " + getMaxVowelsInSubstring("leetcode", 3));
-		System.out.println("rhythms - " + getMaxVowelsInSubstring("rhythms", 4));
-		System.out.println("tryhard - " + getMaxVowelsInSubstring("tryhard", 4));
+		String input = getStringFromFile();
+		long startTime = System.nanoTime();
+		System.out.println("large string - 1" + getMaxVowelsInSubstring(input, 20_000));
+		long stopTime = System.nanoTime();
+		System.out.println("original method time: "+ (stopTime - startTime));
+		
+		startTime = System.nanoTime();
+		System.out.println("large string - 2 " + getMaxVowelsInSubstring2(input, 20_000));
+		stopTime = System.nanoTime();
+		System.out.println("new method time: "+ (stopTime - startTime));
 	}
 
 	private static String getStringFromFile() throws Exception {
